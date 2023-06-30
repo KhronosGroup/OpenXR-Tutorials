a901f3cbbc87732600dd61acd1aac9a6c4ed1562
Andrew Richards <andyjrichards93@gmail.com>
2023-06-30 10:33:53 +0100

    Added Chapter 2.2 code. Multiple Graphics APIs added. Test on Windows only (D3D11, D3D12, OpenGL and Vulkan). WIP.

---
+++ Chapter2.2/GraphicsAPIs.cpp
@@ -0,0 +1,311 @@
+#include "GraphicsAPIs.h"
+
+// D3D11
+#if defined(XR_USE_GRAPHICS_API_D3D11)
+
+#define D3D11_CHECK(x, y)                                                                         \
+    {                                                                                             \
+        HRESULT result = (x);                                                                     \
+        if (FAILED(result)) {                                                                     \
+            std::cout << "ERROR: D3D11: " << std::hex << "0x" << result << std::dec << std::endl; \
+            std::cout << "ERROR: D3D11: " << y << std::endl;                                      \
+        }                                                                                         \
+    }
+
+#define D3D11_SAFE_RELEASE(p) \
+    {                         \
+        if (p) {              \
+            (p)->Release();   \
+            (p) = nullptr;    \
+        }                     \
+    }
+
+GraphicsAPI_D3D11::GraphicsAPI_D3D11(XrInstance xrInstance, XrSystemId systemId) {
+    OPENXR_CHECK(xrGetInstanceProcAddr(xrInstance, "xrGetD3D11GraphicsRequirementsKHR", (PFN_xrVoidFunction *)&xrGetD3D11GraphicsRequirementsKHR), "Failed to get InstanceProcAddr.");
+    XrGraphicsRequirementsD3D11KHR graphicsRequirements{XR_TYPE_GRAPHICS_REQUIREMENTS_D3D11_KHR};
+    OPENXR_CHECK(xrGetD3D11GraphicsRequirementsKHR(xrInstance, systemId, &graphicsRequirements), "Failed to get Graphics Requirements for D3D11.");
+
+    D3D11_CHECK(CreateDXGIFactory1(IID_PPV_ARGS(&factory)), "Failed to create DXGI factory.");
+
+    UINT i = 0;
+    IDXGIAdapter *adapter = nullptr;
+    DXGI_ADAPTER_DESC adapterDesc = {};
+    while (factory->EnumAdapters(i, &adapter) != DXGI_ERROR_NOT_FOUND) {
+        adapter->GetDesc(&adapterDesc);
+        if (adapterDesc.AdapterLuid.LowPart == graphicsRequirements.adapterLuid.LowPart && adapterDesc.AdapterLuid.HighPart == graphicsRequirements.adapterLuid.HighPart) {
+            break;  // We have the matching adapter that OpenXR wants.
+        }
+    }
+
+    D3D11_CHECK(D3D11CreateDevice(adapter, D3D_DRIVER_TYPE_UNKNOWN, 0, 0, &graphicsRequirements.minFeatureLevel, 1, D3D11_SDK_VERSION, &device, nullptr, &immediateContext), "Failed to create D3D11 Device.");
+};
+
+GraphicsAPI_D3D11::~GraphicsAPI_D3D11() {
+    D3D11_SAFE_RELEASE(immediateContext);
+    D3D11_SAFE_RELEASE(device);
+    D3D11_SAFE_RELEASE(factory);
+}
+#endif
+
+// D3D12
+#if defined(XR_USE_GRAPHICS_API_D3D12)
+
+#define D3D12_CHECK(x, y)                                                                         \
+    {                                                                                             \
+        HRESULT result = (x);                                                                     \
+        if (FAILED(result)) {                                                                     \
+            std::cout << "ERROR: D3D12: " << std::hex << "0x" << result << std::dec << std::endl; \
+            std::cout << "ERROR: D3D12: " << y << std::endl;                                      \
+        }                                                                                         \
+    }
+
+#define D3D12_SAFE_RELEASE(p) \
+    {                         \
+        if (p) {              \
+            (p)->Release();   \
+            (p) = nullptr;    \
+        }                     \
+    }
+
+GraphicsAPI_D3D12::GraphicsAPI_D3D12(XrInstance xrInstance, XrSystemId systemId) {
+    OPENXR_CHECK(xrGetInstanceProcAddr(xrInstance, "xrGetD3D12GraphicsRequirementsKHR", (PFN_xrVoidFunction *)&xrGetD3D12GraphicsRequirementsKHR), "Failed to get InstanceProcAddr.");
+    XrGraphicsRequirementsD3D12KHR graphicsRequirements{XR_TYPE_GRAPHICS_REQUIREMENTS_D3D12_KHR};
+    OPENXR_CHECK(xrGetD3D12GraphicsRequirementsKHR(xrInstance, systemId, &graphicsRequirements), "Failed to get Graphics Requirements for D3D12.");
+
+    D3D12_CHECK(CreateDXGIFactory2(0, IID_PPV_ARGS(&factory)), "Failed to create DXGI factory.");
+
+    UINT i = 0;
+    IDXGIAdapter1 *adapter = nullptr;
+    DXGI_ADAPTER_DESC adapterDesc = {};
+    while (factory->EnumAdapters1(i, &adapter) != DXGI_ERROR_NOT_FOUND) {
+        adapter->GetDesc(&adapterDesc);
+        if (adapterDesc.AdapterLuid.LowPart == graphicsRequirements.adapterLuid.LowPart && adapterDesc.AdapterLuid.HighPart == graphicsRequirements.adapterLuid.HighPart) {
+            break;  // We have the matching adapter that OpenXR wants.
+        }
+    }
+
+    D3D12_CHECK(D3D12CreateDevice(adapter, graphicsRequirements.minFeatureLevel, IID_PPV_ARGS(&device)), "Failed to create D3D12 Device.");
+
+    D3D12_COMMAND_QUEUE_DESC commandQueueDesc;
+    commandQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+    commandQueueDesc.Priority = 0;
+    commandQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+    commandQueueDesc.NodeMask = 0;
+    D3D12_CHECK(device->CreateCommandQueue(&commandQueueDesc, IID_PPV_ARGS(&queue)), "Failed to create D3D12 Command Queue.");
+}
+
+GraphicsAPI_D3D12 ::~GraphicsAPI_D3D12() {
+    D3D12_SAFE_RELEASE(device);
+    D3D12_SAFE_RELEASE(queue);
+}
+#endif
+
+// OpenGL
+#if defined(XR_USE_GRAPHICS_API_OPENGL)
+GraphicsAPI_OpenGL::GraphicsAPI_OpenGL(XrInstance xrInstance, XrSystemId systemId) {
+    OPENXR_CHECK(xrGetInstanceProcAddr(xrInstance, "xrGetOpenGLGraphicsRequirementsKHR", (PFN_xrVoidFunction *)&xrGetOpenGLGraphicsRequirementsKHR), "Failed to get InstanceProcAddr.");
+    XrGraphicsRequirementsOpenGLKHR graphicsRequirements{XR_TYPE_GRAPHICS_REQUIREMENTS_OPENGL_KHR};
+    OPENXR_CHECK(xrGetOpenGLGraphicsRequirementsKHR(xrInstance, systemId, &graphicsRequirements), "Failed to get Graphics Requirements for OpenGL.");
+
+    // https://github.com/KhronosGroup/OpenXR-SDK-Source/blob/f122f9f1fc729e2dc82e12c3ce73efa875182854/src/tests/hello_xr/graphicsplugin_opengl.cpp#L103-L121
+    // Initialize the gl extensions. Note we have to open a window.
+    ksDriverInstance driverInstance{};
+    ksGpuQueueInfo queueInfo{};
+    ksGpuSurfaceColorFormat colorFormat{KS_GPU_SURFACE_COLOR_FORMAT_B8G8R8A8};
+    ksGpuSurfaceDepthFormat depthFormat{KS_GPU_SURFACE_DEPTH_FORMAT_D24};
+    ksGpuSampleCount sampleCount{KS_GPU_SAMPLE_COUNT_1};
+    if (!ksGpuWindow_Create(&window, &driverInstance, &queueInfo, 0, colorFormat, depthFormat, sampleCount, 640, 480, false)) {
+        std::cout << "ERROR: OPENGL: Failed to create Context." << std::endl;
+    }
+
+    GLint glMajorVersion = 0;
+    GLint glMinorVersion = 0;
+    glGetIntegerv(GL_MAJOR_VERSION, &glMajorVersion);
+    glGetIntegerv(GL_MINOR_VERSION, &glMinorVersion);
+
+    const XrVersion glApiVersion = XR_MAKE_VERSION(glMajorVersion, glMinorVersion, 0);
+    if (graphicsRequirements.minApiVersionSupported > glApiVersion) {
+        std::cout << "ERROR: OPENGL: The created OpenGL version doesn't meet the minimum requried API version for OpenXR." << std::endl;
+    }
+}
+GraphicsAPI_OpenGL::~GraphicsAPI_OpenGL() {
+    ksGpuWindow_Destroy(&window);
+}
+#endif
+
+// Vulkan
+#if defined(XR_USE_GRAPHICS_API_VULKAN)
+
+#define VULKAN_CHECK(x, y)                                                                         \
+    {                                                                                              \
+        VkResult result = (x);                                                                     \
+        if (result != VK_SUCCESS) {                                                                \
+            std::cout << "ERROR: VULKAN: " << std::hex << "0x" << result << std::dec << std::endl; \
+            std::cout << "ERROR: VULKAN: " << y << std::endl;                                      \
+        }                                                                                          \
+    }
+
+GraphicsAPI_Vulkan::GraphicsAPI_Vulkan(XrInstance xrInstance, XrSystemId systemId) {
+    // Instance
+    LoadPFN_XrFunctions(xrInstance);
+
+    XrGraphicsRequirementsVulkanKHR graphicsRequirements{XR_TYPE_GRAPHICS_REQUIREMENTS_VULKAN_KHR};
+    OPENXR_CHECK(xrGetVulkanGraphicsRequirementsKHR(xrInstance, systemId, &graphicsRequirements), "Failed to get Graphics Requirements for Vulkan.");
+
+    VkApplicationInfo ai;
+    ai.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    ai.pNext = nullptr;
+    ai.pApplicationName = "OpenXR Tutorial - Vulkan";
+    ai.applicationVersion = 1;
+    ai.pEngineName = "OpenXR Tutorial - Vulkan Engine";
+    ai.engineVersion = 1;
+    ai.apiVersion = graphicsRequirements.minApiVersionSupported;
+
+    uint32_t instanceExtensionCount = 0;
+    VULKAN_CHECK(vkEnumerateInstanceExtensionProperties(nullptr, &instanceExtensionCount, nullptr), "Failed to enumerate InstanceExtensionProperties.");
+
+    std::vector<VkExtensionProperties> instanceExtensionProperties;
+    instanceExtensionProperties.resize(instanceExtensionCount);
+    VULKAN_CHECK(vkEnumerateInstanceExtensionProperties(nullptr, &instanceExtensionCount, instanceExtensionProperties.data()), "Failed to enumerate InstanceExtensionProperties.");
+    const std::vector<std::string>& openXrInstanceExtensionNames = GetInstanceExtensionsForOpenXR(xrInstance, systemId);
+    for (const std::string &requestExtension : openXrInstanceExtensionNames) {
+        for (const VkExtensionProperties &extensionProperty : instanceExtensionProperties) {
+            if (strcmp(requestExtension.c_str(), extensionProperty.extensionName))
+                continue;
+            else
+                activeInstanceExtensions.push_back(requestExtension.c_str());
+            break;
+        }
+    }
+
+    VkInstanceCreateInfo instanceCI;
+    instanceCI.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceCI.pNext = nullptr;
+    instanceCI.flags = 0;
+    instanceCI.pApplicationInfo = &ai;
+    instanceCI.enabledLayerCount = 0;
+    instanceCI.ppEnabledLayerNames = nullptr;
+    instanceCI.enabledExtensionCount = static_cast<uint32_t>(activeInstanceExtensions.size());
+    instanceCI.ppEnabledExtensionNames = activeInstanceExtensions.data();
+    VULKAN_CHECK(vkCreateInstance(&instanceCI, nullptr, &instance), "Failed to create Vulkan Instance.");
+
+    // Physical Device
+    uint32_t physicalDeviceCount = 0;
+    std::vector<VkPhysicalDevice> physicalDevices;
+    VULKAN_CHECK(vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, nullptr), "Failed to enumerate PhysicalDevices.");
+    physicalDevices.resize(physicalDeviceCount);
+    VULKAN_CHECK(vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, physicalDevices.data()), "Failed to enumerate PhysicalDevices.");
+
+    VkPhysicalDevice physicalDeviceFromXR;
+    OPENXR_CHECK(xrGetVulkanGraphicsDeviceKHR(xrInstance, systemId, instance, &physicalDeviceFromXR), "Failed to get Graphics Device for Vulkan.");
+    auto physicalDeviceFromXR_it = std::find(physicalDevices.begin(), physicalDevices.end(), physicalDeviceFromXR);
+    if (physicalDeviceFromXR_it != physicalDevices.end()) {
+        physicalDevice = *physicalDeviceFromXR_it;
+    } else {
+        std::cout << "ERROR: Vulkan: Failed to find PhysicalDevice for OpenXR." << std::endl;
+        // Select the first available device.
+        physicalDevice = physicalDevices[0];
+    }
+
+    // Device
+    std::vector<VkQueueFamilyProperties> queueFamilyProperties;
+    uint32_t queueFamilyPropertiesCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyPropertiesCount, nullptr);
+    queueFamilyProperties.resize(queueFamilyPropertiesCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &queueFamilyPropertiesCount, queueFamilyProperties.data());
+
+    std::vector<VkDeviceQueueCreateInfo> deviceQueueCIs;
+    std::vector<std::vector<float>> queuePriorities;
+    queuePriorities.resize(queueFamilyProperties.size());
+    deviceQueueCIs.resize(queueFamilyProperties.size());
+    for (size_t i = 0; i < deviceQueueCIs.size(); i++) {
+        for (size_t j = 0; j < queueFamilyProperties[i].queueCount; j++)
+            queuePriorities[i].push_back(1.0f);
+
+        deviceQueueCIs[i].sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        deviceQueueCIs[i].pNext = nullptr;
+        deviceQueueCIs[i].flags = 0;
+        deviceQueueCIs[i].queueFamilyIndex = static_cast<uint32_t>(i);
+        deviceQueueCIs[i].queueCount = queueFamilyProperties[i].queueCount;
+        deviceQueueCIs[i].pQueuePriorities = queuePriorities[i].data();
+    }
+
+    uint32_t deviceExtensionCount = 0;
+    VULKAN_CHECK(vkEnumerateDeviceExtensionProperties(physicalDevice, 0, &deviceExtensionCount, 0), "Failed to enumerate DeviceExtensionProperties.");
+    std::vector<VkExtensionProperties> deviceExtensionProperties;
+    deviceExtensionProperties.resize(deviceExtensionCount);
+
+    VULKAN_CHECK(vkEnumerateDeviceExtensionProperties(physicalDevice, 0, &deviceExtensionCount, deviceExtensionProperties.data()), "Failed to enumerate DeviceExtensionProperties.");
+    const std::vector<std::string> &openXrDeviceExtensionNames = GetDeviceExtensionsForOpenXR(xrInstance, systemId);
+    for (const std::string &requestExtension : openXrDeviceExtensionNames) {
+        for (const VkExtensionProperties &extensionProperty : deviceExtensionProperties) {
+            if (strcmp(requestExtension.c_str(), extensionProperty.extensionName))
+                continue;
+            else
+                activeDeviceExtensions.push_back(requestExtension.c_str());
+            break;
+        }
+    }
+
+    VkPhysicalDeviceFeatures features;
+    vkGetPhysicalDeviceFeatures(physicalDevice, &features);
+
+    VkDeviceCreateInfo deviceCI;
+    deviceCI.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    deviceCI.pNext = nullptr;
+    deviceCI.flags = 0;
+    deviceCI.queueCreateInfoCount = static_cast<uint32_t>(deviceQueueCIs.size());
+    deviceCI.pQueueCreateInfos = deviceQueueCIs.data();
+    deviceCI.enabledLayerCount = 0;
+    deviceCI.ppEnabledLayerNames = nullptr;
+    deviceCI.enabledExtensionCount = static_cast<uint32_t>(activeDeviceExtensions.size());
+    deviceCI.ppEnabledExtensionNames = activeDeviceExtensions.data();
+    deviceCI.pEnabledFeatures = &features;
+    VULKAN_CHECK(vkCreateDevice(physicalDevice, &deviceCI, nullptr, &device), "Failed to create Device.");
+
+    queueFamilyIndex = 0;
+    queueIndex = 0;
+}
+
+GraphicsAPI_Vulkan::~GraphicsAPI_Vulkan() {
+}
+
+void GraphicsAPI_Vulkan::LoadPFN_XrFunctions(XrInstance xrInstance) {
+    OPENXR_CHECK(xrGetInstanceProcAddr(xrInstance, "xrGetVulkanGraphicsRequirementsKHR", (PFN_xrVoidFunction *)&xrGetVulkanGraphicsRequirementsKHR), "Failed to get InstanceProcAddr.");
+    OPENXR_CHECK(xrGetInstanceProcAddr(xrInstance, "xrGetVulkanInstanceExtensionsKHR", (PFN_xrVoidFunction *)&xrGetVulkanInstanceExtensionsKHR), "Failed to get InstanceProcAddr.");
+    OPENXR_CHECK(xrGetInstanceProcAddr(xrInstance, "xrGetVulkanDeviceExtensionsKHR", (PFN_xrVoidFunction *)&xrGetVulkanDeviceExtensionsKHR), "Failed to get InstanceProcAddr.");
+    OPENXR_CHECK(xrGetInstanceProcAddr(xrInstance, "xrGetVulkanGraphicsDeviceKHR", (PFN_xrVoidFunction *)&xrGetVulkanGraphicsDeviceKHR), "Failed to get InstanceProcAddr.");
+}
+
+std::vector<std::string> GraphicsAPI_Vulkan::GetInstanceExtensionsForOpenXR(XrInstance xrInstance, XrSystemId systemId) {
+    uint32_t extensionNamesSize = 0;
+    OPENXR_CHECK(xrGetVulkanInstanceExtensionsKHR(xrInstance, systemId, 0, &extensionNamesSize, nullptr), "Failed to get Vulkan Instance Extensions.");
+
+    std::vector<char> extensionNames(extensionNamesSize);
+    OPENXR_CHECK(xrGetVulkanInstanceExtensionsKHR(xrInstance, systemId, extensionNamesSize, &extensionNamesSize, extensionNames.data()), "Failed to get Vulkan Instance Extensions.");
+
+    std::stringstream streamData(extensionNames.data());
+    std::vector<std::string> extensions;
+    std::string extension;
+    while (std::getline(streamData, extension, ' ')) {
+        extensions.push_back(extension);
+    }
+    return extensions;
+}
+
+std::vector<std::string> GraphicsAPI_Vulkan::GetDeviceExtensionsForOpenXR(XrInstance xrInstance, XrSystemId systemId) {
+    uint32_t extensionNamesSize = 0;
+    OPENXR_CHECK(xrGetVulkanDeviceExtensionsKHR(xrInstance, systemId, 0, &extensionNamesSize, nullptr), "Failed to get Vulkan Device Extensions.");
+
+    std::vector<char> extensionNames(extensionNamesSize);
+    OPENXR_CHECK(xrGetVulkanDeviceExtensionsKHR(xrInstance, systemId, extensionNamesSize, &extensionNamesSize, extensionNames.data()), "Failed to get Vulkan Device Extensions.");
+
+    std::stringstream streamData(extensionNames.data());
+    std::vector<std::string> extensions;
+    std::string extension;
+    while (std::getline(streamData, extension, ' ')) {
+        extensions.push_back(extension);
+    }
+    return extensions;
+}
+#endif
---
+++ Chapter2.2/GraphicsAPIs.h
@@ -0,0 +1,151 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+// Platform headers
+#if defined(_WIN32)
+#define WIN32_LEAN_AND_MEAN
+#include "Windows.h"
+#include "unknwn.h"
+#define XR_USE_PLATFORM_WIN32
+
+// #define XR_USE_GRAPHICS_API_D3D11
+// #define XR_USE_GRAPHICS_API_D3D12
+#define XR_USE_GRAPHICS_API_OPENGL
+// #define XR_USE_GRAPHICS_API_VULKAN
+#endif
+
+#if defined(__LINIX__)
+#define XR_USE_PLATFORM_XLIB
+#define XR_USE_PLATFORM_XCB
+#define XR_USE_PLATFORM_WAYLAND
+
+#define XR_USE_GRAPHICS_API_OPENGL
+#define XR_USE_GRAPHICS_API_VULKAN
+#endif
+
+#if defined(__ANDROID__)
+#include "android_native_app_glue.h"
+#define XR_USE_PLATFORM_ANDROID
+
+#define XR_USE_GRAPHICS_API_OPENGL_ES
+#define XR_USE_GRAPHICS_API_VULKAN
+#endif
+
+// Graphic APIs headers
+#if defined(XR_USE_GRAPHICS_API_D3D11)
+#include <d3d11.h>
+#endif
+
+#if defined(XR_USE_GRAPHICS_API_D3D12)
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#endif
+
+#if defined(XR_USE_GRAPHICS_API_OPENGL)
+// gfxwrapper will redefine these macros
+#undef XR_USE_PLATFORM_WIN32
+#undef XR_USE_PLATFORM_XLIB
+#undef XR_USE_PLATFORM_XCB
+#undef XR_USE_PLATFORM_WAYLAND
+#include "gfxwrapper_opengl.h"
+#endif
+
+#if defined(XR_USE_GRAPHICS_API_VULKAN)
+#include "vulkan/vulkan.h"
+#endif
+
+// OpenXR
+#include "openxr/openxr.h"
+#include "openxr/openxr_platform.h"
+
+#if defined(XR_USE_GRAPHICS_API_D3D11)
+class GraphicsAPI_D3D11 {
+public:
+    GraphicsAPI_D3D11(XrInstance xrInstance, XrSystemId systemId);
+    ~GraphicsAPI_D3D11();
+
+public:
+    IDXGIFactory1* factory = nullptr;
+    ID3D11Device* device = nullptr;
+    ID3D11DeviceContext* immediateContext = nullptr;
+
+private:
+    PFN_xrGetD3D11GraphicsRequirementsKHR xrGetD3D11GraphicsRequirementsKHR = nullptr;
+};
+#endif
+
+#if defined(XR_USE_GRAPHICS_API_D3D12)
+class GraphicsAPI_D3D12 {
+public:
+    GraphicsAPI_D3D12(XrInstance xrInstance, XrSystemId systemId);
+    ~GraphicsAPI_D3D12();
+
+public:
+    IDXGIFactory4* factory = nullptr;
+    ID3D12Device* device = nullptr;
+    ID3D12CommandQueue* queue = nullptr;
+
+private:
+    PFN_xrGetD3D12GraphicsRequirementsKHR xrGetD3D12GraphicsRequirementsKHR = nullptr;
+};
+#endif
+
+#if defined(XR_USE_GRAPHICS_API_OPENGL)
+class GraphicsAPI_OpenGL {
+public:
+    GraphicsAPI_OpenGL(XrInstance xrInstance, XrSystemId systemId);
+    ~GraphicsAPI_OpenGL();
+
+    ksGpuWindow window{};
+
+private:
+    PFN_xrGetOpenGLGraphicsRequirementsKHR xrGetOpenGLGraphicsRequirementsKHR = nullptr;
+};
+#endif
+
+#if defined(XR_USE_GRAPHICS_API_VULKAN)
+class GraphicsAPI_Vulkan {
+public:
+    GraphicsAPI_Vulkan(XrInstance xrInstance, XrSystemId systemId);
+    ~GraphicsAPI_Vulkan();
+
+private:
+    void LoadPFN_XrFunctions(XrInstance xrInstance);
+    std::vector<std::string> GetInstanceExtensionsForOpenXR(XrInstance xrInstance, XrSystemId systemId);
+    std::vector<std::string> GetDeviceExtensionsForOpenXR(XrInstance xrInstance, XrSystemId systemId);
+
+public:
+    VkInstance instance{};
+    VkPhysicalDevice physicalDevice{};
+    VkDevice device{};
+    uint32_t queueFamilyIndex = 0;
+    uint32_t queueIndex = 0;
+
+    std::vector<const char*> activeInstanceExtensions;
+    std::vector<const char*> activeDeviceExtensions;
+
+private:
+    PFN_xrGetVulkanGraphicsRequirementsKHR xrGetVulkanGraphicsRequirementsKHR = nullptr;
+    PFN_xrGetVulkanInstanceExtensionsKHR xrGetVulkanInstanceExtensionsKHR = nullptr;
+    PFN_xrGetVulkanDeviceExtensionsKHR xrGetVulkanDeviceExtensionsKHR = nullptr;
+    PFN_xrGetVulkanGraphicsDeviceKHR xrGetVulkanGraphicsDeviceKHR = nullptr;
+};
+#endif
+
+#if defined(XR_USE_GRAPHICS_API_OPENGL_ES)
+class GraphicsAPI_OpenGLES {
+};
+#endif
+
+// XR_DOCS_TAG_BEGIN_Helper_Functions0
+#define OPENXR_CHECK(x, y)                                                       \
+    {                                                                            \
+        XrResult result = (x);                                                   \
+        if (!XR_SUCCEEDED(result)) {                                             \
+            std::cout << "ERROR: OPENXR: (" << result << ") " << y << std::endl; \
+        }                                                                        \
+    }
+// XR_DOCS_TAG_END_Helper_Functions0
