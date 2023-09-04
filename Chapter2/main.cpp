4341ad1e23885a39b172cb450a26b594b1e7bcb9
Andrew Richards <andyjrichards93@gmail.com>
2023-09-04 16:08:49 +0100

    Updated finding Vulkan package in CMakeLists.txt files. XR_RUNTIME_JSON in Chapter2/main.cpp in wrapped behind #ifdef __linux__. Other minor corrections.

--- Chapter2/main.cpp
+++ Chapter2/main.cpp
@@ -72,9 +72,11 @@ private:
             // XR_DOCS_TAG_END_instanceExtensions
         }
 
+#if defined(__linux__) && !defined(__ANDROID__)
         SetEnv("XR_RUNTIME_JSON", "/home/roderick/monado/build/openxr_monado-dev.json");
         std::string XR_RUNTIME_JSON = GetEnv("XR_RUNTIME_JSON");
         std::cout << "XR_RUNTIME_JSON" << XR_RUNTIME_JSON << "\n";
+#endif
 
         // XR_DOCS_TAG_BEGIN_find_apiLayer_extension
         // Get all the API Layers from the OpenXR runtime.
--- Common/GraphicsAPI.cpp
+++ Common/GraphicsAPI.cpp
@@ -10,7 +10,7 @@ bool CheckGraphicsAPI_TypeIsValidForPlatform(GraphicsAPI_Type type) {
 #if defined(XR_USE_PLATFORM_ANDROID) || defined(XR_USE_PLATFORM_XCB) || defined(XR_USE_PLATFORM_WAYLAND)
     return (type == OPENGL_ES) || (type == VULKAN);
 #endif
-	return false;
+    return false;
 }
 
 const char *GetGraphicsAPIInstanceExtensionString(GraphicsAPI_Type type) {
@@ -20,24 +20,24 @@ const char *GetGraphicsAPIInstanceExtensionString(GraphicsAPI_Type type) {
     }
 #endif
 #if defined(XR_USE_GRAPHICS_API_D3D12)
-	if (type == D3D12) {
+    if (type == D3D12) {
         return XR_KHR_D3D12_ENABLE_EXTENSION_NAME;
     }
 #endif
 #if defined(XR_USE_GRAPHICS_API_OPENGL)
-	if (type == OPENGL) {
+    if (type == OPENGL) {
         return XR_KHR_OPENGL_ENABLE_EXTENSION_NAME;
     }
 #endif
 #if defined(XR_USE_GRAPHICS_API_OPENGL_ES)
-	if (type == OPENGL_ES) {
+    if (type == OPENGL_ES) {
         return XR_KHR_OPENGL_ES_ENABLE_EXTENSION_NAME;
-	} 
+    }
 #endif
 #if defined(XR_USE_GRAPHICS_API_VULKAN)
     if (type == VULKAN) {
         return XR_KHR_VULKAN_ENABLE_EXTENSION_NAME;
-	}
+    }
 #endif
     std::cerr << "ERROR: Unknown Graphics API." << std::endl;
     DEBUG_BREAK;
