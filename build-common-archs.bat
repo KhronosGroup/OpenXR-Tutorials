e27f02d2b10e67477f893aeb326a697f76b0822e
Andrew Richards <andyjrichards93@gmail.com>
2023-10-27 13:36:49 +0100

    Added build scripts for creating archives of the Common folder.

---
+++ build-common-archs.bat
@@ -0,0 +1,113 @@
+::echo off
+
+rmdir /s /q build\common_archs
+mkdir build\common_archs
+
+:LOOP
+IF "%~1"=="D3D11" GOTO D3D11
+IF "%~1"=="D3D12" GOTO D3D12
+IF "%~1"=="OPENGL" GOTO OPENGL
+IF "%~1"=="OPENGL_ES" GOTO OPENGL_ES
+IF "%~1"=="VULKAN" GOTO VULKAN
+IF "%~1"=="" GOTO END
+
+rem D3D11
+:D3D11
+tar -a -cf build\common_archs\Common_D3D11.zip ^
+    Common/GraphicsAPI.cpp ^
+    Common/GraphicsAPI_D3D11.cpp ^
+    Common/OpenXRDebugUtils.cpp ^
+    Common/DebugOutput.h ^
+    Common/GraphicsAPI.h ^
+    Common/GraphicsAPI_D3D11.h ^
+    Common/HelperFunctions.h ^
+    Common/OpenXRDebugUtils.h ^
+    Common/OpenXRHelper.h
+
+SHIFT
+GOTO LOOP
+
+rem D3D12
+:D3D12
+tar -a -cf build\common_archs\Common_D3D12.zip ^
+    Common/GraphicsAPI.cpp ^
+    Common/GraphicsAPI_D3D12.cpp ^
+    Common/OpenXRDebugUtils.cpp ^
+    Common/DebugOutput.h ^
+    Common/GraphicsAPI.h ^
+    Common/GraphicsAPI_D3D12.h ^
+    Common/HelperFunctions.h ^
+    Common/OpenXRDebugUtils.h ^
+    Common/OpenXRHelper.h
+
+SHIFT
+GOTO LOOP
+
+rem OPENGL
+:OPENGL
+tar -a -cf build\common_archs\Common_OpenGL.zip ^
+    Common/GraphicsAPI.cpp ^
+    Common/GraphicsAPI_OpenGL.cpp ^
+    Common/OpenXRDebugUtils.cpp ^
+    Common/DebugOutput.h ^
+    Common/GraphicsAPI.h ^
+    Common/GraphicsAPI_OpenGL.h ^
+    Common/HelperFunctions.h ^
+    Common/OpenXRDebugUtils.h ^
+    Common/OpenXRHelper.h
+
+SHIFT
+GOTO LOOP
+
+rem OPENGL_ES
+:OPENGL_ES
+tar -a -cf build\common_archs\Common_OpenGL_ES.zip ^
+    Common/GraphicsAPI.cpp ^
+    Common/GraphicsAPI_OpenGL_ES.cpp ^
+    Common/OpenXRDebugUtils.cpp ^
+    Common/DebugOutput.h ^
+    Common/GraphicsAPI.h ^
+    Common/GraphicsAPI_OpenGL_ES.h ^
+    Common/HelperFunctions.h ^
+    Common/OpenXRDebugUtils.h ^
+    Common/OpenXRHelper.h
+
+SHIFT
+GOTO LOOP
+
+rem VULKAN
+:VULKAN
+tar -a -cf build\common_archs\Common_Vulkan.zip ^
+    Common/GraphicsAPI.cpp ^
+    Common/GraphicsAPI_Vulkan.cpp ^
+    Common/OpenXRDebugUtils.cpp ^
+    Common/DebugOutput.h ^
+    Common/GraphicsAPI.h ^
+    Common/GraphicsAPI_Vulkan.h ^
+    Common/HelperFunctions.h ^
+    Common/OpenXRDebugUtils.h ^
+    Common/OpenXRHelper.h
+
+SHIFT
+GOTO LOOP
+
+rem Full Folder
+:END
+tar -a -cf build\common_archs\Common.zip ^
+    Common/GraphicsAPI.cpp ^
+    Common/GraphicsAPI_D3D11.cpp ^
+    Common/GraphicsAPI_D3D12.cpp ^
+    Common/GraphicsAPI_OpenGL.cpp ^
+    Common/GraphicsAPI_OpenGL_ES.cpp ^
+    Common/GraphicsAPI_Vulkan.cpp ^
+    Common/OpenXRDebugUtils.cpp ^
+    Common/DebugOutput.h ^
+    Common/GraphicsAPI.h ^
+    Common/GraphicsAPI_D3D11.h ^
+    Common/GraphicsAPI_D3D12.h ^
+    Common/GraphicsAPI_OpenGL.h ^
+    Common/GraphicsAPI_OpenGL_ES.h ^
+    Common/GraphicsAPI_Vulkan.h ^
+    Common/HelperFunctions.h ^
+    Common/OpenXRDebugUtils.h ^
+    Common/OpenXRHelper.h
---
+++ build-common-archs.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+rm -rf build/common_archs
+mkdir -p build/common_archs
+
+for api in "$@"
+do
+    if [[ "$api" == "D3D11" ]]; then
+        # D3D11
+        echo "$api"
+        zip -r build/common_archs/Common_D3D11.zip \
+            Common/GraphicsAPI.cpp \
+            Common/GraphicsAPI_D3D11.cpp \
+            Common/OpenXRDebugUtils.cpp \
+            Common/DebugOutput.h \
+            Common/GraphicsAPI.h \
+            Common/GraphicsAPI_D3D11.h \
+            Common/HelperFunctions.h \
+            Common/OpenXRDebugUtils.h \
+            Common/OpenXRHelper.h \
+            /
+    fi
+    if [[ "$api" == "D3D12" ]]; then
+        # D3D12
+        echo "$api"
+        zip -r build/common_archs/Common_D3D12.zip \
+            Common/GraphicsAPI.cpp \
+            Common/GraphicsAPI_D3D12.cpp \
+            Common/OpenXRDebugUtils.cpp \
+            Common/DebugOutput.h \
+            Common/GraphicsAPI.h \
+            Common/GraphicsAPI_D3D12.h \
+            Common/HelperFunctions.h \
+            Common/OpenXRDebugUtils.h \
+            Common/OpenXRHelper.h \
+            /
+    fi
+    if [[ "$api" == "OPENGL" ]]; then
+        # OPENGL
+        echo "$api"
+        zip -r build/common_archs/Common_OpenGL.zip \
+            Common/GraphicsAPI.cpp \
+            Common/GraphicsAPI_OpenGL.cpp \
+            Common/OpenXRDebugUtils.cpp \
+            Common/DebugOutput.h \
+            Common/GraphicsAPI.h \
+            Common/GraphicsAPI_OpenGL.h \
+            Common/HelperFunctions.h \
+            Common/OpenXRDebugUtils.h \
+            Common/OpenXRHelper.h \
+            /
+    fi
+    if [[ "$api" == "OPENGL_ES" ]]; then
+        # OPENGL_ES
+        echo "$api"
+        zip -r build/common_archs/Common_OpenGL_ES.zip \
+            Common/GraphicsAPI.cpp \
+            Common/GraphicsAPI_OpenGL_ES.cpp \
+            Common/OpenXRDebugUtils.cpp \
+            Common/DebugOutput.h \
+            Common/GraphicsAPI.h \
+            Common/GraphicsAPI_OpenGL_ES.h \
+            Common/HelperFunctions.h \
+            Common/OpenXRDebugUtils.h \
+            Common/OpenXRHelper.h \
+            /
+    fi
+    if [[ "$api" == "VULKAN" ]]; then
+        # VULKAN
+        echo "$api"
+        zip -r build/common_archs/Common_Vulkan.zip \
+            Common/GraphicsAPI.cpp \
+            Common/GraphicsAPI_Vulkan.cpp \
+            Common/OpenXRDebugUtils.cpp \
+            Common/DebugOutput.h \
+            Common/GraphicsAPI.h \
+            Common/GraphicsAPI_Vulkan.h \
+            Common/HelperFunctions.h \
+            Common/OpenXRDebugUtils.h \
+            Common/OpenXRHelper.h \
+            /
+    fi
+done
+
+# Full Folder
+zip -r build/common_archs/Common.zip \
+    Common/GraphicsAPI.cpp \
+    Common/GraphicsAPI_D3D11.cpp \
+    Common/GraphicsAPI_D3D12.cpp \
+    Common/GraphicsAPI_OpenGL.cpp \
+    Common/GraphicsAPI_OpenGL_ES.cpp \
+    Common/GraphicsAPI_Vulkan.cpp \
+    Common/OpenXRDebugUtils.cpp \
+    Common/DebugOutput.h \
+    Common/GraphicsAPI.h \
+    Common/GraphicsAPI_D3D11.h \
+    Common/GraphicsAPI_D3D12.h \
+    Common/GraphicsAPI_OpenGL.h \
+    Common/GraphicsAPI_OpenGL_ES.h \
+    Common/GraphicsAPI_Vulkan.h \
+    Common/HelperFunctions.h \
+    Common/OpenXRDebugUtils.h \
+    Common/OpenXRHelper.h \
